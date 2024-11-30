11b0d936de0a53de90b85dd19045fd09cca428ec
emnsxyz24 <edymikhaelx24@gmail.com>
2024-11-30 13:51:29 +0700

    adding necessary card component

---
+++ client/src/components/LandingPageCard.jsx
@@ -0,0 +1,52 @@
+import penIcon from "../assets/icons/pen.png";
+import webIcon from "../assets/icons/webprog.png";
+import videoIcon from "../assets/icons/video-editor.png";
+import voiceIcon from "../assets/icons/voice-over.png";
+
+const LandingPagecard = () => {
+  return (
+    <div className="flex flex-wrap justify-center gap-2">
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg mx-auto p-5" src={penIcon} alt="" width={180} />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Design Grafis
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg mx-auto p-5" src={webIcon} alt="" width={180} />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Website & Pemrograman
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg mx-auto p-5" src={videoIcon} alt="" width={180} />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Video Editor
+            </h5>
+          </div>
+        </a>
+      </div>
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a href="#">
+          <img className="rounded-t-lg mx-auto p-5" src={voiceIcon} alt="" width={180} />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Voice Over
+            </h5>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+export default LandingPagecard;
---
+++ client/src/components/LandingPageSecCard.jsx
@@ -0,0 +1,67 @@
+import pricing from "../assets/icons/pricing.png";
+import categories from "../assets/icons/categories.png";
+import fastTime from "../assets/icons/fast-time.png";
+
+const LandingPageSecCard = () => {
+  return (
+    <div className="flex flex-wrap justify-center gap-2">
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div>
+          <img
+            className="rounded-t-lg mx-auto p-5"
+            src={categories}
+            alt=""
+            width={180}
+          />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Banyak Kategori
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Temukan freelancer berpengalaman untuk segala jenis tugas, dengan
+              berbagai anggaran.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div href="#">
+          <img
+            className="rounded-t-lg mx-auto p-5"
+            src={pricing}
+            alt=""
+            width={180}
+          />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Harga Transparan
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Bayar per proyek, dengan pembayaran yang hanya dilepaskan setelah
+              Anda setujui.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex text-center justify-center items-center w-[20rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div href="#">
+          <img
+            className="rounded-t-lg mx-auto p-5"
+            src={fastTime}
+            alt=""
+            width={180}
+          />
+          <div className="p-5">
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Hasil Cepat
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Gunakan filter untuk menemukan freelancer yang tepat dengan cepat, dan dapatkan hasil kerja berkualitas tepat waktu.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LandingPageSecCard;
