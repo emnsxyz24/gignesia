e4847885fe8b038deec1275a9aac98243a85fcda
emnsxyz24 <edymikhaelx24@gmail.com>
2024-12-15 00:57:42 +0700

    adding route for needed page

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,38 +1,176 @@
-import {createBrowserRouter} from "react-router-dom";
-// import Home from "../pages/Home";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ErrorPage from "../pages/Error-pages";
 import LandingPageNonUser from "../pages/LandingPageNonUser";
 import LandingPageUser from "../pages/LandingPageUser";
 import About from "../pages/About";
+import PageClient from "../pages/customer/FreelancerList";
+import PageFreelancer from "../pages/freelancer/PageFreelancer";
+import ClientProfile from "../pages/customer/ClientProfile";
+import FreelancerProfile from "../pages/freelancer/Profile";
+import FreelancerDetails from "../pages/customer/FreelancerDetails";
+import Dashboard from "../pages/freelancer/Dashboard";
+import Orders from "../pages/freelancer/Orders";
+import ManageServices from "../pages/freelancer/ManageServices";
+import { useAuth } from "../context/AuthContext";
+import { Loading } from "../components/Loadings";
+import MoneyManagement from "../pages/freelancer/MoneyManagement";
 
+const ProtectedRoute = ({
+  children,
+  allowedRoles = [],
+  redirectIfAuthenticated = false,
+  landingPage = false,
+  clientElement = null,
+  freelancerElement = null,
+}) => {
+  const { isAuthenticated, user, authLoading } = useAuth();
+  if (authLoading) {
+    return <Loading />;
+  }
+
+  if (redirectIfAuthenticated) {
+    return isAuthenticated ? <Navigate to="/" replace /> : children;
+  }
+
+  if (redirectIfAuthenticated) {
+    return children;
+  }
+
+  if (!isAuthenticated && authLoading) {
+    return <ErrorPage />;
+  }
+
+  if (landingPage) {
+    return isAuthenticated ? <LandingPageUser /> : <LandingPageNonUser />;
+  }
+
+  if(user.role){
+    if (allowedRoles.length > 0 && !allowedRoles.includes(user.role)) {
+      console.error('Access denied: User role not allowed', { 
+        userRole: user.role, 
+        allowedRoles ,
+      });
+      return <ErrorPage />;
+    }
+  }
+
+  if (user?.role === "client" && clientElement) {
+    return clientElement;
+  }
+
+  if (user?.role === "freelancer" && freelancerElement) {
+    return freelancerElement;
+  }
+
+  return children;
+};
 
 export const router = createBrowserRouter([
-
-    {
-        path: "/login",
-        element: <Login/>,
-        errorElement: <ErrorPage/>
-    },
-    {
-      path: "/",
-      element: <LandingPageNonUser/>,
-      errorElement: <ErrorPage/>  
-    },
-    {
-        path: "/about",
-        element: <About/>,
-        errorElement: <ErrorPage/>
-    },
-    {
-        path: "/user",
-        element: <LandingPageUser/>, 
-        errorElement: <ErrorPage/>
-    },
-    {
-        path: "/register",
-        element: <Register/>,
-        errorElement: <ErrorPage/>
-    }
-])  
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute landingPage={true}>
+        <LandingPageUser />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/register",
+    element: (
+      <ProtectedRoute redirectIfAuthenticated={true}>
+        <Register />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: (
+      <ProtectedRoute redirectIfAuthenticated={true}>
+        <Login />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/freelancers",
+    element: (
+      <ProtectedRoute>
+        <PageClient />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/cariclient",
+    element: (
+      <ProtectedRoute>
+        <PageFreelancer />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedRoute
+        clientElement={<ClientProfile />}
+        freelancerElement={<FreelancerProfile />}
+      />
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: `/details/:serviceId`,
+    element: (
+      <ProtectedRoute>
+        <FreelancerDetails />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute allowedRoles={["freelancer"]}>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/orders",
+    element: (
+      <ProtectedRoute allowedRoles={["freelancer"]}>
+        <Orders />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/services",
+    element: (
+      <ProtectedRoute allowedRoles={["freelancer"]}>
+        <ManageServices />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/management",
+    element: (
+      <ProtectedRoute allowedRoles={["freelancer"]}>
+        <MoneyManagement />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+]);
