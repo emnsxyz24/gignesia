862117d7a2e735fa8d4a2a7843de2e734abad770
Anthaaa <miftahzarkasihanantha@gmail.com>
2024-12-15 18:25:40 +0700

    add api for review

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,6 +4,7 @@ import category from "./categoryRoutes.js";
 import service from "./serviceRoutes.js";
 import user from "./userRoutes.js";
 import order from "./orderRoutes.js";
+import reviewRoutes from "./reviewRoutes.js";
 
 const routes = express.Router();
 
@@ -13,5 +14,6 @@ routes.use(category);
 routes.use(service);
 routes.use(user);
 routes.use(order);
+routes.use(reviewRoutes);
 
 export default routes;
